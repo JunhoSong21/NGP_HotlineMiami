23337de01b7ba86cae2dca0c736d5eff3683ce8b
JunhoSong21 <junhoasd1zxc2@gmail.com>
2025-11-10 15:01:24 +0900

    패킷 이름 정수비교처리

--- NGP_Server/Packet.h
+++ NGP_Server/Packet.h
@@ -1,17 +1,19 @@
 #pragma once
-constexpr int PACKET_TYPE_SIZE = 14;
+
 
 struct PacketHeader {
-	char packetType[PACKET_TYPE_SIZE];
-	unsigned short size;
+	int packetType;
+	size_t packetSize;
 };
 
+constexpr int SC_PLAYER_MOVE = 1000;
 struct SC_PLAYER_MOVE {
 	float posX;		// X 좌표
 	float posY;		// Y 좌표
 	float atan2;	// 회전값
 };
 
+constexpr int SC_BULLET_STATE = 1001;
 struct SC_BULLET_STATE {
 	bool isVisible;	// 활성 여부
 	float posX;		// X 좌표
@@ -20,6 +22,7 @@ struct SC_BULLET_STATE {
 	float dirY;		// Y 벡터
 };
 
+constexpr int SC_GRENADE_STATE = 1002;
 struct SC_GRENADE_STATE {
 	bool isVisible;	// 활성 여부
 	float posX;		// X 좌표
@@ -29,6 +32,7 @@ struct SC_GRENADE_STATE {
 	bool isExploded;// 폭발 여부
 };
 
+constexpr int SC_GAME_END = 1003;
 struct SC_GAME_END {
 	bool isMatchEnded;	// 게임 종료 여부
 };
