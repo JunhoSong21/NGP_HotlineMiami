bc7def7d6a8af7e44cd78c37c4c35f71b3a0992c
yunsol17 <yunsol1007@naver.com>
2025-11-14 02:14:39 +0900

    Send_Input()에 send 추가

--- HotlineMiami/ClientNetwork.cpp
+++ HotlineMiami/ClientNetwork.cpp
@@ -28,6 +28,13 @@ int Send_Input(SOCKET sock, HWND hWnd, const Player& player)
     pkt.mouseX = static_cast<float>(mouse.x);
     pkt.mouseY = static_cast<float>(mouse.y);
 
-    return 0;
+    // 4) 패킷을 char 버퍼로 복사
+    char buffer[sizeof(pkt)];
+    std::memcpy(buffer, &pkt, sizeof(pkt));
+
+    // 5) 서버로 전송
+    int retval = send(sock, buffer, sizeof(buffer), 0);
+
+    return retval;
 
 }
--- HotlineMiami/pch.h
+++ HotlineMiami/pch.h
@@ -1,3 +1,7 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
 // 기본 헤더
 #include <Windows.h>
 
