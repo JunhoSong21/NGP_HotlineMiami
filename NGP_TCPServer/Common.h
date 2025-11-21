2d5fc9186b44e894c640670baf8f57eca07a34c6
JunhoSong21 <junhoasd1zxc2@gmail.com>
2025-11-21 18:05:52 +0900

    이중선언 오류 해결중

--- NGP_TCPServer/Common.h
+++ NGP_TCPServer/Common.h
@@ -1,8 +1,8 @@
-pragma once
 #define _CRT_SECURE_NO_WARNINGS // 구형 C 함수 사용 시 경고 끄기
 #define _WINSOCK_DEPRECATED_NO_WARNINGS // 구형 소켓 API 사용 시 경고 끄기
 constexpr int SERVERPORT	= 9000;
 constexpr int BUFSIZE		= 1024;	// BUFSIZE 공용사용
+
 
 #include <winsock2.h> // 윈속2 메인 헤더
 #include <ws2tcpip.h> // 윈속2 확장 헤더
---
+++ NGP_TCPServer/NetworkThread.cpp
@@ -0,0 +1,74 @@
+#include "NetworkThread.h"
+
+NetworkThread::NetworkThread(int id, SOCKET clientSock) :
+	threadId(id), clientSock(clientSock)
+{
+	networkThread = std::thread(&NetworkThread::ThreadFunc, this);
+	printf("Client %d Thread Start\n", threadId);
+}
+
+NetworkThread::~NetworkThread()
+{
+	if (networkThread.joinable())
+		networkThread.join();
+
+	closesocket(clientSock);
+}
+
+void NetworkThread::ThreadFunc()
+{
+	int retValue = 0;
+	PacketHeader packetHeader{};
+
+	while (true) {
+		retValue = recv(clientSock, (char*)&packetHeader, sizeof(packetHeader), MSG_WAITALL);
+		if (retValue == SOCKET_ERROR) {
+			err_display("recv()");
+			break;
+		}
+
+		switch (packetHeader.packetType) {
+		case PN::CS_KEY_INPUT:
+			CS_KEY_INPUT keyinputPacket;
+			retValue = recv(clientSock, (char*)&keyinputPacket, sizeof(keyinputPacket), MSG_WAITALL);
+			if (retValue == SOCKET_ERROR)
+				err_display("recv() Ket Input");
+			else
+				KeyInputPacketProcess(keyinputPacket);
+			break;
+		case PN::CS_BULLET_TRIGGER:
+			CS_BULLET_TRIGGER bulletTriggerPacket;
+			retValue = recv(clientSock, (char*)&bulletTriggerPacket, sizeof(bulletTriggerPacket), MSG_WAITALL);
+			if (retValue == SOCKET_ERROR)
+				err_display("recv() BulletTrigger");
+			else
+				BulletTriggerPacketProcess(bulletTriggerPacket);
+			break;
+		case PN::CS_GRENADE_THROW:
+			break;
+		case PN::CS_LOGIN_PACKET:
+			break;
+		case PN::CS_ROOM_PACKET:
+			break;
+		}
+	}
+}
+
+void NetworkThread::KeyInputPacketProcess(struct CS_KEY_INPUT packet)
+{
+	auto playerMoveEvent = std::make_unique<PlayerMove>(threadId, 0.0, 0.0);
+	EventQueue::GetInstance().PushEvent(std::move(playerMoveEvent));
+	printf("Key Input Packet recv\n");
+}
+
+void NetworkThread::BulletTriggerPacketProcess(struct CS_BULLET_TRIGGER packet)
+{
+	auto bulletTriggerEvent = std::make_unique<BulletTrigger>(threadId, 0.0, 0.0);
+	EventQueue::GetInstance().PushEvent(std::move(bulletTriggerEvent));
+}
+
+void NetworkThread::GrenadeThrowPacketProcess(struct CS_GRENADE_THROW packet)
+{
+	auto grenadeThrowEvent = std::make_unique<GrenadeThrow>(threadId, 0, 0, 0.0);
+	EventQueue::GetInstance().PushEvent(std::move(grenadeThrowEvent));
+}
