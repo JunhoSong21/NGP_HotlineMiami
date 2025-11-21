f25b9339946158892a6902ad9931c6a0ca10acc8
JunhoSong21 <junhoasd1zxc2@gmail.com>
2025-11-21 14:35:05 +0900

    게임이벤트 수정

--- NGP_TCP_Server/GameEvent.h
+++ NGP_TCP_Server/GameEvent.h
@@ -5,8 +5,10 @@ struct GameEvent {
 		PLAYER_MOVE,
 		BULLET_TRIGGER,
 		BULLET_UPDATE,
+		BULLET_COLLISION,
 		GRENADE_THROW,
 		GRENADE_UPDATE,
+		GRENADE_EXPLOSION,
 
 		LOGIN_TRY,
 		ROOM_MAKE,
@@ -31,11 +33,36 @@ struct PlayerMove : GameEvent {
 };
 
 struct BulletTrigger : GameEvent {
+	float posX, posY, atan2;
 
+	BulletTrigger(int threadId, float x, float y, float angle) :
+		posX(x), posY(y), atan2(angle)
+	{
+		type = GameEvent::Type::BULLET_TRIGGER;
+		networkThreadId = threadId;
+	}
 };
 
 struct BulletUpdate : GameEvent {
+	float destX, destY;
 
+	BulletUpdate(int threadId, float x, float y) :
+		destX(x), destY(y)
+	{
+		type = GameEvent::Type::BULLET_UPDATE;
+		networkThreadId = threadId;
+	}
+};
+
+struct BulletCollision : GameEvent {
+	bool isActive;
+
+	BulletCollision(int threadId, bool isCollision) :
+		isActive(isCollision)
+	{
+		type = GameEvent::Type::BULLET_COLLISION;
+		networkThreadId = threadId;
+	}
 };
 
 struct GrenadeThrow : GameEvent {
@@ -43,6 +70,10 @@ struct GrenadeThrow : GameEvent {
 };
 
 struct GrenadeUpdate : GameEvent {
+
+};
+
+struct GrenadeExplosion : GameEvent {
 
 };
 
