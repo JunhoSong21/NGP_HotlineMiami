a81defe35537183d7b0f22a0524179b66e11b3a8
yunsol17 <yunsol1007@naver.com>
2025-12-11 19:00:52 +0900

    플레이어 아이디에 따라서 초기 위치 설정

--- NGP_TCP_Server/NetworkThread.cpp
+++ NGP_TCP_Server/NetworkThread.cpp
@@ -41,6 +41,26 @@ void NetworkThread::LoginProcess()
 			printf("loginPacket recv Error\n");
 
 		unique_ptr<Player> newPlayer = make_unique<Player>(threadId);
+		switch (threadId)
+		{
+		case 0:
+			newPlayer->SetPosX(218.0f);
+			newPlayer->SetPosY(230.0f);
+			break;
+		case 1:
+			newPlayer->SetPosX(300.0f);
+			newPlayer->SetPosY(550.0f);
+			break;
+		case 2:
+			newPlayer->SetPosX(810.0f);
+			newPlayer->SetPosY(370.0f);
+			break;
+		default:
+			// ±âº»°ª
+			newPlayer->SetPosX(0.0f);
+			newPlayer->SetPosY(0.0f);
+			break;
+		}
 		DataManager::GetInstance().AddPlayer(std::move(newPlayer));
 
 		unique_ptr<Bullet> newBullet = make_unique<Bullet>(threadId);
